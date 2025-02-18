d5659529c8d794555bfe7597964bab141d9b7850
phamcongthanhvn2k6 <phamcongt56@gmail.com>
2025-02-18 09:49:33 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "function.h" 
+#include "function.h"
+#include "datatype.h"
 #define maxlop 100 
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	menu();
-	return 0;
+    menu();
+    return 0;
 }
